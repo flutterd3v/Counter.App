14617fe4d4e5a0c40ec03d2e364aae3af574159c
Fatima_ <107647711+FotimaNurmuhammad@users.noreply.github.com>
2022-06-30 16:29:16 +0500

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage_ extends StatefulWidget {
+  const MyHomePage_({Key? key}) : super(key: key);
+  static final id = 'MyHomePage';
+
+  @override
+  State<MyHomePage_> createState() => _MyHomePage_State();
+}
+
+class _MyHomePage_State extends State<MyHomePage_> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Counter',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          count.toString(),
+          style: TextStyle(color: Colors.blueAccent, fontSize: 35),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+      ),
+    );
+  }
+}
